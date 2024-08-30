de879330e3e41276eb1ce69d9efff79ea25e33cd
whiterven <163173007+whiterven@users.noreply.github.com>
2024-08-30 07:51:22 +0000

    Create transactionStore.ts

---
+++ app/stores/transactionStore.ts
@@ -0,0 +1,16 @@
+// app/stores/transactionStore.ts
+import { create } from 'zustand';
+
+interface TransactionStore {
+  isModalOpen: boolean;
+  currentAction: string;
+  setIsModalOpen: (isOpen: boolean) => void;
+  setCurrentAction: (action: string) => void;
+}
+
+export const useTransactionStore = create<TransactionStore>((set) => ({
+  isModalOpen: false,
+  currentAction: '',
+  setIsModalOpen: (isOpen) => set({ isModalOpen: isOpen }),
+  setCurrentAction: (action) => set({ currentAction: action }),
+}));
